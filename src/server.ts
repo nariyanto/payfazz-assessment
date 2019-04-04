6cd9aa928d528df21fb05088b68972e04677c577
Septiyan Nariyanto <septiyan.nariyanto@gmail.com>
2019-04-04 17:55:09 +0700

    Error handling.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,17 @@ import http from "http";
 import express from "express";
 import { applyMiddleware } from "./utils";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 const app = express();
 applyMiddleware(middleware, app);
@@ -9,6 +20,8 @@ applyMiddleware(middleware, app);
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
+
+applyMiddleware(errorHandlers, app);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(app);
